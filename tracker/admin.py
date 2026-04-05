b4342e444e9a2c943fae3da7e28b43e294dd96bd
kev <knvasquez@mail.fhsu.edu>
2026-04-05 12:56:58 -0500

    Add Category and Expense models and configure Django admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+# INF601 - Advanced Programming in Python
 
-# Register your models here.
+# Kevin Vasquez
+
+# Mini Project 4
+
+from django.contrib import admin
+from .models import Category, Expense
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    search_fields = ('name',)
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'amount', 'date', 'category', 'user')
+    list_filter = ('date', 'category')
+    search_fields = ('title', 'notes')
