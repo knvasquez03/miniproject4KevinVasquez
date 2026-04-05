91db93d587cbb43d2a4f7e74dce9810c38800241
kev <knvasquez@mail.fhsu.edu>
2026-04-05 13:18:04 -0500

    added new directories for expense_tracker. added templates and tracker. then added HTML files

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -3,9 +3,11 @@
 # Kevin Vasquez
 
 # Mini Project 4
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tracker.urls')),
 ]
